24286755d9d72c67535a12de3ff2824c68a3527f
Okulov Vadim <Vedroidua@gmail.com>
2020-12-24 13:25:04 +0200

    Changed logic

--- src/main/java/ua/vedroid/elevator/Floor.java
+++ src/main/java/ua/vedroid/elevator/Floor.java
@@ -1,67 +1,46 @@
 package ua.vedroid.elevator;
 
-import java.util.Comparator;
-import java.util.LinkedList;
+import java.util.List;
 
 public class Floor {
-    private final int floorNumber;
-    private LinkedList<Passenger> passengers;
+    private final int number;
+    //private LinkedList<Passenger> passengers;
+    private List<Passenger> passengers;
     private boolean upButton;
     private boolean downButton;
     
-    public Floor(int floorNumber, LinkedList<Passenger> passengers) {
-        this.floorNumber = floorNumber;
+    public Floor(int number, List<Passenger> passengers) {
+        this.number = number;
         this.passengers = passengers;
         
-        this.passengers.removeIf(passenger -> passenger.getRequiredFloor() == floorNumber);
-        this.passengers.sort(Comparator.comparingInt(Passenger::getRequiredFloor));
+        this.passengers.removeIf(passenger -> passenger.getRequiredFloor() == number);
+        //this.passengers.sort(Comparator.comparingInt(Passenger::getRequiredFloor));
         updateButtons();
     }
     
-    public Passenger getPassengerFromTheBeginning() {
-        Passenger passenger = passengers.pollFirst();
+    public void removePassenger(Passenger passenger) {
+        passengers.remove(passenger);
         updateButtons();
-        return passenger;
-    }
-    
-    public Passenger getPassengerFromTheEnd() {
-        Passenger passenger = passengers.pollLast();
-        updateButtons();
-        return passenger;
-    }
-    
-    public Passenger checkPassengerFromTheBeginning() {
-        if (passengers.isEmpty()) {
-            return null;
-        }
-        return passengers.peekFirst();
-    }
-    
-    public Passenger checkPassengerFromTheEnd() {
-        if (passengers.isEmpty()) {
-            return null;
-        }
-        return passengers.peekLast();
     }
     
     private void updateButtons() {
         upButton = false;
         downButton = false;
         for (Passenger passenger : passengers) {
-            if (passenger.getRequiredFloor() > floorNumber) {
+            if (passenger.getRequiredFloor() > number) {
                 upButton = true;
             }
-            if (passenger.getRequiredFloor() < floorNumber) {
+            if (passenger.getRequiredFloor() < number) {
                 downButton = true;
             }
         }
     }
     
-    public int getFloorNumber() {
-        return floorNumber;
+    public int getNumber() {
+        return number;
     }
     
-    public LinkedList<Passenger> getPassengers() {
+    public List<Passenger> getPassengers() {
         return passengers;
     }
     
@@ -79,6 +58,6 @@ public class Floor {
     
     @Override
     public String toString() {
-        return "#" + floorNumber + " " + passengers;
+        return "#" + number + " " + passengers;
     }
 }
